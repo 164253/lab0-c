1743613f32bd55772b1d553ebb99708c446efd30
164253 <jeremydingeric@gmail.com>
2024-02-29 13:42:08 +0800

    Replace the implementation of q_remove_tail
    Same as q_insert_tail, we can easily implement q_remove_tail
    with q_remove_head(head->prev, s).

--- queue.c
+++ queue.c
@@ -68,13 +68,7 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head || list_empty(head))
-        return NULL;
-    element_t *elem = list_last_entry(head, element_t, list);
-    if (sp)
-        strncpy(sp, elem->value, bufsize - 1);
-    list_del(head->prev);
-    return elem;
+    return q_remove_head(head->prev, sp, bufsize);
 }
 
 /* Return number of elements in queue */
